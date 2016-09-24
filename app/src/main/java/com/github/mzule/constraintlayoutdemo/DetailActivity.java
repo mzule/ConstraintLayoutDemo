8498d3859e2a97263fce0a47a075bc69a90250dc
caodongping <caodongping@baidu.com>
2016-09-24 10:16:53 +0800

    first commit

---
+++ app/src/main/java/com/github/mzule/constraintlayoutdemo/DetailActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package com.github.mzule.constraintlayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CaoDongping on 24/09/2016.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
